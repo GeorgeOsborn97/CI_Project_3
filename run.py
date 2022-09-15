491126824f36395b69ceaa733ed82ef72a260ac3
GeorgeOsborn97 <George.b.osborn@outlook.com>
2022-09-15 15:56:15 +0000

    add dictionaries to be utilised later, add refinment to the confirm race loop

--- run.py
+++ run.py
@@ -15,6 +15,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('project3_test_sheet')
 
+your_character = {
+    'Race': '',
+    'Class': '',
+    'Level': '',
+    'Hit points': '',
+}    
+your_ability_scores = {  
+    'Strength': '',
+    'Dexterity': '',
+    'Constitution': '',
+    'Intellegence': '',
+    'wisdom': '',
+    'Charisma': '',
+}
+your_skills_and_proficiencies = {
+    'proficency_modifier': '',
+}
+your_feats_and_traits = {
+
+}
+your_spells_and_attacks = {
+
+}
+
 races = SHEET.worksheet('Races')
 race = races.get_all_values()
 print('Welcome to the Dungeons and Drgaons character creator!')
@@ -63,15 +87,17 @@ def confirm_race(prompt):
     return input(prompt)
 
 
-confirmed_race = confirm_race(f'Are you sure you want to choose {chosen_race}? ')
-while confirmed_race != 'yes':
-    if confirmed_race == 'yes':
+confirmed_race = confirm_race(f'Are you sure you want to choose {chosen_race}? Please answer "Yes" or "No"')
+while confirmed_race != 'Yes':
+    if confirmed_race == 'Yes':
         print(f'{chosen_race} confirmed!')
-    else:  
+    elif confirmed_race == 'No':  
         xy = 1
         print('change decision')
         first_choice()
-        confirmed_race = confirm_race(f'Are you sure you want to choose {chosen_race}? ')
+        confirmed_race = confirm_race(f'Are you sure you want to choose {chosen_race}? Please answer "Yes" or "No"')
+    else:
+        print('Please only type "Yes" or "No".')
+        confirmed_race = confirm_race(f'Are you sure you want to choose {chosen_race}? Please answer "Yes" or "No"')
 
 print('end of step one')
-
