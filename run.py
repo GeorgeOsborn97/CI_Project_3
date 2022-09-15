bf02fb7bf7551bd033aa1958517da246594e8ddc
GeorgeOsborn97 <George.b.osborn@outlook.com>
2022-09-15 13:37:12 +0000

    install loop and error message for unplayable races

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
+"""
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -20,29 +20,32 @@ race = races.get_all_values()
 print('Welcome to the Dungeons and Drgaons character creator!')
 print('To begin please choose from one of the following races.')
 print(race)
+xy = 1
 
+while xy == 1:
+    def select_race(prompt):
+        """
+        Return the users chosen race
+        """
+        return input(prompt)
 
-def select_race(prompt):
-    """
-    Return the users chosen race
-    """
-    return input(prompt)
+    chosen_race = select_race('Type a race here to see their traits and starting abilities: ')
+    print(chosen_race)
 
+    def pull_racial_traits(choosen_race):
+        """
+        Pull the relevent racial traits
+        """
+        global xy
+        try:
+            trait_sheet = SHEET.worksheet(choosen_race)
+            racial_traits = trait_sheet.get_all_values()
+            print(racial_traits)
+            xy += 1
 
-chosen_race = select_race('Type a race here to see their traits and starting abilities: ')
-print(chosen_race)
+        except Exception:
+            print(f'{choosen_race} is not a playable Race, please select again.')
 
+    pull_racial_traits(chosen_race)
 
-def pull_racial_traits():
-    """
-    Pull the relevent racial traits
-    """
-    trait_sheet = SHEET.worksheet('Human')
-    racial_traits = trait_sheet.get_all_values()
-    if chosen_race == 'Human':
-        print(racial_traits)
-    else:
-        print('not human')
-
-
-pull_racial_traits()
+print('out the loop')
